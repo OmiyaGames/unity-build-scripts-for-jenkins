f3204381b1c2a0b3858cce08446b3356e61f8c8f
Taro Omiya <taro.omiya@omiyagames.com>
2014-07-19 23:49:46 -0400

    Changing zippin algorithm back to zip again

--- jenkinsBuildScript.bat
+++ jenkinsBuildScript.bat
@@ -2,7 +2,7 @@ REM Switch to the argument's directory
 cd %1
 
 REM Zip all the build folders
-FOR /D %%G IN (Builds\*) DO "C:\Program Files\7-Zip\7z.exe" a "%%G.7z" ".\%%G\*"
+FOR /D %%G IN (Builds\*) DO "C:\Program Files\7-Zip\7z.exe" a "%%G.zip" ".\%%G\*"
 
 REM Use mercurial to tag the built revision.
 hg tag -f --rev %MERCURIAL_REVISION_NUMBER% "[Auto-build] %JOB_NAME% %BUILD_DISPLAY_NAME%"
