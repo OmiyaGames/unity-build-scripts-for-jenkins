a0968c21de17cb6ae34d494e99e3d33e11403d26
taro <taro@localhost>
2014-07-24 15:05:26 -0400

    Adding comments as a reminder to the intent of the build operation.

--- jenkinsBuildScript.bat
+++ jenkinsBuildScript.bat
@@ -1,9 +1,12 @@
 REM Switch to the argument's directory
-cd %1
+REM cd %1
 
 REM Zip all the build folders
 FOR /D %%G IN (Builds\*) DO "C:\Program Files\7-Zip\7z.exe" a "%%G.zip" ".\%%G\*"
 
 REM Use mercurial to tag the built revision.
+REM hg up --clean autobuild
+REM hg merge default
+REM hg commit -m %1
 hg tag -f --rev %MERCURIAL_REVISION_NUMBER% "[Auto-build] %JOB_NAME% %BUILD_DISPLAY_NAME%"
 hg push
