a08aaa03d43a2cb67d939c0339ce02cc5bb5a46c
Taro Omiya <taro.omiya@omiyagames.com>
2014-08-01 01:15:19 -0400

    Adding conditional to check if the autobuild branch exists

--- updateAutoBuildBranch.rb
+++ updateAutoBuildBranch.rb
@@ -2,16 +2,41 @@
 #require 'IO'
 
 # Setup constant values
-MERCURIAL = 'C:\Program Files\TortoiseHg\hg.exe'
-BRANCH = 'autobuild'
+BRANCH_NAME = 'autobuild'
 REVISION = ARGV[0]
 TAG = ARGV[1]
 
-# For debugging, print all values
-puts BRANCH
-puts REVISION
-puts TAG
+# Run "hg branches"
+all_branches = IO.popen(['hg', 'branches'])
 
-# Check all the branches
-allbranches = IO.popen([MERCURIAL, 'branches'])
-puts allbranches.readlines
+# Read the output
+does_branch_autobuild_exist = false
+for branch in all_branches.readlines
+  
+  # Check if branch autobuild exists
+  if branch.start_with? BRANCH_NAME
+  
+    # If it does, indicate so
+    does_branch_autobuild_exist = true
+    break
+  end
+end
+
+# Close the command
+all_branches.close
+
+system("hg up testing")
+
+# Check if branch autobuild exists
+if does_branch_autobuild_exist
+  
+  # If it does, switch to this branch
+  #system("hg up #{BRANCH_NAME}")
+  
+  # Merge the changes from default, up to revision
+  #system("hg up #{BRANCH_NAME}")
+else
+end
+
+p does_branch_autobuild_exist
+
