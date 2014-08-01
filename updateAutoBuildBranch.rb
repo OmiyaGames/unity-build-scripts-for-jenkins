58c59c971a07d1694a8dca85467c75235ee04b53
taro <taro@localhost>
2014-08-01 00:06:49 -0400

    Working on updating the autobuild branch after post build.

--- updateAutoBuildBranch.rb
+++ updateAutoBuildBranch.rb
@@ -1,6 +1,23 @@
-if ARGV.argv.length >= 2
-  puts "Revision:"
-  puts ARGV.argv[0]
-  puts "Tag name:"
-  puts ARGV.argv[1]
+# Import mercurial
+require 'win32-process'
+require 'mercurial-ruby'
+
+# Configure Mercurial to the proper location
+Mercurial.configure do |conf|
+  conf.hg_binary_path = 'C:\Program Files\TortoiseHg\hg.exe'
 end
+
+# Setup constant values
+BRANCH = 'autobuild'
+DIRECTORY = ARGV[0]
+REVISION = ARGV[1]
+TAG = ARGV[2]
+
+# For debugging, print all values
+puts BRANCH
+puts DIRECTORY
+puts REVISION
+puts TAG
+
+# Create a new repository
+repository = Mercurial::Repository.create(DIRECTORY)
