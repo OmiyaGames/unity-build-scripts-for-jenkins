1607af530554da47275fe36ca3fe3e478417ed72
Taro Omiya <taro.omiya@omiyagames.com>
2014-08-01 00:41:38 -0400

    Working on updating the autobuild branch after post build.

--- updateAutoBuildBranch.rb
+++ updateAutoBuildBranch.rb
@@ -1,23 +1,17 @@
-# Import mercurial
-require 'win32/process'
-require 'mercurial-ruby'
-
-# Configure Mercurial to the proper location
-Mercurial.configure do |conf|
-  conf.hg_binary_path = 'C:\Program Files\TortoiseHg\hg.exe'
-end
+# Import modules
+#require 'IO'
 
 # Setup constant values
+MERCURIAL = 'C:\Program Files\TortoiseHg\hg.exe'
 BRANCH = 'autobuild'
-DIRECTORY = ARGV[0]
-REVISION = ARGV[1]
-TAG = ARGV[2]
+REVISION = ARGV[0]
+TAG = ARGV[1]
 
 # For debugging, print all values
 puts BRANCH
-puts DIRECTORY
 puts REVISION
 puts TAG
 
-# Create a new repository
-repository = Mercurial::Repository.create(DIRECTORY)
+# Check all the branches
+allbranches = IO.popen([MERCURIAL, 'branch'])
+puts allbranches.readlines
