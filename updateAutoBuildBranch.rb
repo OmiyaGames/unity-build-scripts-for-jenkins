c03e6345d6e3d4c576375ef87fe82dc7166c5a5a
taro <taro@localhost>
2014-08-01 02:01:59 -0400

    Fixing a few hg commands

--- updateAutoBuildBranch.rb
+++ updateAutoBuildBranch.rb
@@ -4,13 +4,16 @@ REVISION = ARGV[0]
 TAG_MESSAGE = ARGV[1]
 
 # Define helper methods
+def print_message(message)
+	system("echo \"#{message}\"")
+end
 def run_command(command)
-	puts "> #{command}"
+	print_message(command)
 	system(command)
 end
 
 # Run "hg branches"
-puts '> hg branches'
+print_message('> hg branches')
 all_branches = IO.popen(['hg', 'branches'])
 
 # Read the output
@@ -18,18 +21,18 @@ does_branch_autobuild_exist = false
 for branch in all_branches.readlines
 
 	# Check if branch autobuild exists
-	puts branch
-	if branch.start_with? BRANCH_NAME
+	print_message(branch)
+	if branch.start_with?(BRANCH_NAME)
 
 		# If it does, indicate so
-		puts '* Branch autobuild found!'
+		print_message('* Branch autobuild found!')
 		does_branch_autobuild_exist = true
 		break
 	end
 end
 
 # Close the command
-puts '* Stop branches process'
+print_message('* Stop branches process')
 all_branches.close
 
 # Check if branch autobuild exists
@@ -39,7 +42,7 @@ if does_branch_autobuild_exist
 	run_command("hg up #{BRANCH_NAME}")
 
 	# Merge the changes from default, up to revision
-	run_command("hg merge -r #{REVISION} default")
+	run_command("hg merge -r #{REVISION}")
 
 	# Commit the changes from default, up to revision
 	run_command("hg commit -m \"Merging the default branch up to revision #{REVISION}.\"")
@@ -55,13 +58,5 @@ end
 # Tag this revision
 run_command("hg tag -f \"#{TAG_MESSAGE}\"")
 
-# Check if branch autobuild exists
-if does_branch_autobuild_exist
-
-	# Push changes
-	run_command("hg push")
-else
-
-	# Push changes to the new branch
-	run_command("hg push --new-branch -b #{BRANCH_NAME}")
-end
+# Push changes to the new branch
+run_command("hg push --new-branch -b #{BRANCH_NAME}")
