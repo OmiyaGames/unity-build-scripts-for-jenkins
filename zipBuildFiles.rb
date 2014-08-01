fd2ef903bbd0f830aa5ae863b574ebf90b9e339a
taro <taro@localhost>
2014-08-01 03:03:06 -0400

    One step closer to zipping script working

--- zipBuildFiles.rb
+++ zipBuildFiles.rb
@@ -5,8 +5,16 @@ require 'zip'
 Dir.chdir(ARGV[0])
 
 # Find all directories in this folder
-for file in Dir['*']
-	if File.directory?(file)
-		puts file
+for directory in Dir['*']
+	
+	# Check if file is a directory
+	if File.directory?(directory)
+		
+		Zip::File.open("#{directory}.zip", Zip::File::CREATE) do |zipfile|
+			Dir[File.join(directory, '**', '**')].each do |file|
+				zipfile.add(file, file)
+			end
+		end
+		puts directory
 	end
 end
