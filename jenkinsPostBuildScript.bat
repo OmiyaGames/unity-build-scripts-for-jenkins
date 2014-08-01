bb70dbd668d19014f2dcde3b9d3d12a837b256ec
taro <taro@localhost>
2014-08-01 02:21:47 -0400

    Adding script for zipping

--- jenkinsPostBuildScript.bat
+++ jenkinsPostBuildScript.bat
@@ -1,5 +1,6 @@
 REM Zip all the build folders
-FOR /D %%G IN (Builds\*) DO "C:\Program Files\7-Zip\7z.exe" a "%%G.zip" ".\%%G\*"
+REM FOR /D %%G IN (Builds\*) DO "C:\Program Files\7-Zip\7z.exe" a "%%G.zip" ".\%%G\*"
+ruby C:\repo\BuildScripts.hg\zipBuildFiles.rb "%WORKSPACE%\Builds"
 
 REM Switch to and update the autobuild branch
 ruby C:\repo\BuildScripts.hg\updateAutoBuildBranch.rb %MERCURIAL_REVISION% "[Auto-build] %JOB_NAME% %BUILD_DISPLAY_NAME%"
---
+++ zipBuildFiles.rb
@@ -0,0 +1,4 @@
+# Setup constant values
+BUILD_DIRECTORY = ARGV[0]
+
+puts BUILD_DIRECTORY
