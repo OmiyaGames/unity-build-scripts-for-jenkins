593a5e72014a8a95281b7bd9838ecf093cbb4e98
taro <taro@localhost>
2014-08-01 03:17:41 -0400

    Removing a comment from the batch script.

--- jenkinsPostBuildScript.bat
+++ jenkinsPostBuildScript.bat
@@ -1,5 +1,4 @@
 REM Zip all the build folders
-REM FOR /D %%G IN (Builds\*) DO "C:\Program Files\7-Zip\7z.exe" a "%%G.zip" ".\%%G\*"
 ruby C:\repo\BuildScripts.hg\zipBuildFiles.rb "%WORKSPACE%\Builds"
 
 REM Switch to and update the autobuild branch
