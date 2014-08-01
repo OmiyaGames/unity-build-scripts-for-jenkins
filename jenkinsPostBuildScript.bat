c0a3e564ef76b75b7795ddac470e5bb9663a11b4
Taro Omiya <taro.omiya@omiyagames.com>
2014-08-01 01:47:10 -0400

    Adding tagging and solid branch commands to ruby script.

--- jenkinsPostBuildScript.bat
+++ jenkinsPostBuildScript.bat
@@ -1,9 +1,5 @@
 REM Zip all the build folders
-REM FOR /D %%G IN (Builds\*) DO "C:\Program Files\7-Zip\7z.exe" a "%%G.zip" ".\%%G\*"
-
-REM Use mercurial to tag the built revision.
-REM hg tag -f --rev %MERCURIAL_REVISION% "[Auto-build] %JOB_NAME% %BUILD_DISPLAY_NAME%"
-REM hg push
+FOR /D %%G IN (Builds\*) DO "C:\Program Files\7-Zip\7z.exe" a "%%G.zip" ".\%%G\*"
 
 REM Switch to and update the autobuild branch
-ruby C:\repo\BuildScripts.hg\updateAutoBuildBranch.rb "%WORKSPACE%" %MERCURIAL_REVISION% "[Auto-build] %JOB_NAME% %BUILD_DISPLAY_NAME%"
+ruby C:\repo\BuildScripts.hg\updateAutoBuildBranch.rb %MERCURIAL_REVISION% "[Auto-build] %JOB_NAME% %BUILD_DISPLAY_NAME%"
