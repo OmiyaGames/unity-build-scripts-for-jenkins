f7a8bcdd28fe5791f204a16c790bb7f78359170d
taro <taro@localhost>
2014-10-04 15:16:21 -0400

    Updating the post build batch script to use the new FTP upload ruby script.

--- jenkinsPostBuildScript.bat
+++ jenkinsPostBuildScript.bat
@@ -8,7 +8,10 @@ REM Switch to and update the autobuild branch
 ruby "%SCRIPT_LOCATION%\updateAutoBuildBranch.rb" %MERCURIAL_REVISION% "[Auto-build] %JOB_NAME% %BUILD_DISPLAY_NAME%"
 
 REM Check if any FTP information is provided
+if [%3] == [] goto :DONE
 
 REM If so, upload the webplayer to FTP, if there are any
-REM ruby "%SCRIPT_LOCATION%\ftpUploadWebplayer.rb" "%WORKSPACE%\Builds" url username password %BUILD_DISPLAY_NAME%
-REM ruby "%SCRIPT_LOCATION%\ftpUploadWebplayer.rb" "%WORKSPACE%\Builds" url username password %BUILD_DISPLAY_NAME% directory
+ruby "%SCRIPT_LOCATION%\ftpUploadWebplayer.rb" "%WORKSPACE%\Builds" %1 %2 %3 %BUILD_DISPLAY_NAME%
+
+:DONE
+REM Finished post-build script
